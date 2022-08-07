5a3f8758fa3b7272a7fcbce2f0feb21214612955
Nicolas Brambilla <nbrambilla024@alumnos.iua.edu.ar>
2022-08-07 18:16:00 -0300

    Adding basic auth

--- src/main/java/iua/edu/ar/SecurityConfig.java
+++ src/main/java/iua/edu/ar/SecurityConfig.java
@@ -1,20 +1,43 @@
 package iua.edu.ar;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.inMemoryAuthentication()
-			.withUser("user").password("123").roles("USER")
+			.withUser("user").password(passwordEncoder().encode("123")).roles("USER")
 			.and()
-			.withUser("admin").password("1234").roles("ADMIN");
+			.withUser("admin").password(passwordEncoder().encode("1234")).roles("ADMIN");
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.csrf().disable()
+			.authorizeRequests()
+			.antMatchers("/admin/**").hasRole("ADMIN")
+			.antMatchers("/anonymous*").anonymous()
+			.antMatchers("/login*").permitAll()
+			.antMatchers("/logout*").permitAll()
+			.anyRequest().authenticated()
+			.and()
+			.httpBasic();	
 	}
 
 }
