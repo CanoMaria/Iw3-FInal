9ed8e928a0feac3fba4d93dd6df9b7bf4b38b025
Nicolas Brambilla <nbrambilla024@alumnos.iua.edu.ar>
2022-08-08 22:26:33 -0300

    Fixing rol and user

--- src/main/java/iua/edu/ar/model/User.java
+++ src/main/java/iua/edu/ar/model/User.java
@@ -4,24 +4,56 @@ import java.io.Serializable;
 import java.util.Collection;
 import java.util.Set;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+
 
 @Entity
 @Table(name = "users")
 public class User implements Serializable, UserDetails{
 	
 	private static final long serialVersionUID = 7687287245581692665L;
-	public Integer id;
-	public String nombre;
-	public String email;
-	public String apellido;
-	public String username;
-	public String password;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(length = 80, nullable = false)
+	private String nombre;
+
+	@Column(length = 300, nullable = false, unique = true)
+	private String email;
+
+	@Column(length = 80, nullable = false)
+	private String apellido;
+
+	@Column(length = 30, nullable = false, unique = true)
+	private String username;
+
+	@Column(length = 100)
+	private String password;
+	
+	@ManyToOne
+	@JoinColumn(name="id_rol_principal")
 	public Rol rolPrincipal;
+	
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(name = "users_roles",
+			joinColumns = {@JoinColumn(name = "id_user", referencedColumnName = "id")},
+			inverseJoinColumns = {@JoinColumn(name="id_rol", referencedColumnName = "id")}
+	)
 	public Set<Rol> roles;
 	
 	public Integer getId() {
