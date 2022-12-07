5e193d03e50c9803434a9d8666ca8325edef7e3f
Brambilla Gabriel Nicolás <nbrambilla024@alumnos.iua.edu.ar>
2022-12-07 02:31:53 -0300

    Revert "Revert "Updating frontend, fixing cisternado issue""

--- vue-frontend/src/components/OrdenByEstado.vue
+++ vue-frontend/src/components/OrdenByEstado.vue
@@ -17,26 +17,15 @@
                 <th>Fecha Recepcion</th>
                 <th>Fecha Recepcion Pesaje Inicial</th>
                 <th>Fecha Fin de Carga</th>
-
                 <th>[Cliente]Razon social</th>
                 <th>[Cliente]Contacto</th>
-
-
                 <th>[Producto]Nombre</th>
-
                 <th>[Camion]Patente</th>
-
                 <th>[Camion]Cisternado</th>
-
-
                 <th>[Chofer]Nombre</th>
                 <th>[Chofer]Apellido</th>
                 <th>[Chofer]Documento</th>
-
-
                 <th>Estado de la orden</th>
-
-
             </thead>
             <tbody>
                 <tr v-for="orden in ordenes.filter(orden => orden.estado == estadoInput)" v-bind:key="orden.id">
@@ -44,25 +33,15 @@
                     <td> {{ orden.fechaRecepcion }} </td>
                     <td> {{ orden.fechaRecepcionPesajeInicial }} </td>
                     <td> {{ orden.fechaFinCarga }} </td>
-
                     <td> {{ orden.cliente.razonSocial }} </td>
                     <td> {{ orden.cliente.contacto }} </td>
-
-
                     <td> {{ orden.producto.nombre }} </td>
-
                     <td> {{ orden.camion.patente }} </td>
                     <td> {{ orden.camion.cisternado }} </td>
-
-
                     <td> {{ orden.chofer.nombre }} </td>
                     <td> {{ orden.chofer.apellido }} </td>
                     <td> {{ orden.chofer.documento }} </td>
-
-
                     <td> {{ orden.estado }} </td>
-
-
                 </tr>
             </tbody>
         </table>
